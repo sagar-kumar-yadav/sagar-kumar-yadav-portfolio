20146a165919a51b903fd60ad45facd0c06844cc
sagar-kumar-yadav <sk5323912@gmail.com>
2023-10-17 14:13:09 +0530

    new commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,152 +5,6 @@ import { Link } from "react-scroll";
 
 const Navbar = ({ toggleMenu, isOpen }) => {
   return (
-    // <header className="header">
-    //   <div className="w-full items-center justify-between py-2 px-4 bg-white fixed top-0 z-10">
-    //     <div className="flex items-center justify-between w-full">
-    //       <div className="header_logo_and_name_cont">
-    //         <div className="header_logo_img_container">
-    //           <img
-    //             src={logo}
-    //             alt="sagar yadav logo img"
-    //             className="header_logo_img"
-    //           />
-    //         </div>
-    //         <span className="header_logo_name">Sagar Yadav</span>
-    //       </div>
-
-    //       {/* -------------------------header menu----------------------------------------------------- */}
-    //       <div className="header_menu">
-    //         <Link
-    //           activeClass="active"
-    //           to="home"
-    //           spy={true}
-    //           smooth={true}
-    //           offset={-100}
-    //           duration={500}
-    //           className="header_link"
-    //         >
-    //           Home
-    //         </Link>
-
-    //         <Link
-    //           activeClass="active"
-    //           to="about"
-    //           spy={true}
-    //           smooth={true}
-    //           offset={-10}
-    //           duration={500}
-    //           className="header_link"
-    //         >
-    //           About
-    //         </Link>
-
-    //         <Link
-    //           activeClass="projects"
-    //           to="projects"
-    //           spy={true}
-    //           smooth={true}
-    //           offset={-5}
-    //           duration={500}
-    //           className="header_link"
-    //         >
-    //           Projects
-    //         </Link>
-
-    //         <Link
-    //           activeClass="contact"
-    //           to="contact"
-    //           spy={true}
-    //           smooth={true}
-    //           offset={-10}
-    //           duration={500}
-    //           className="header_link"
-    //         >
-    //           Contact
-    //         </Link>
-    //       </div>
-
-    //       {/* -------------------------header hamburg  menu------------------------------------------- */}
-    //       <div className=" md:hidden flex flex-col justify-center items-center ">
-    //         <button onClick={toggleMenu}>
-    //           <img
-    //             src="./src/assets/svg/ham-menu.svg"
-    //             alt="hamburg menu"
-    //             className={`w-[100%] ${isOpen ? "hidden" : "block"}`}
-    //           />
-
-    //           <img
-    //             src="./src/assets/svg/ham-menu-close.svg"
-    //             alt="hamburg menu"
-    //             className={`w-[100%] ${isOpen ? "block" : "hidden"}`}
-    //           />
-    //         </button>
-    //       </div>
-    //     </div>
-
-    //     <div
-    //       className={`${isOpen ? "block " : "hidden"} md:hidden `}
-    //       id="mobile-menu"
-    //     >
-    //       <div className="flex flex-col gap-4 text-md text-gray-700 dark:text-neutral-200 float-right width-[100%]">
-    //         <div className="flex flex-col gap-4 bg-white">
-    //           <Link
-    //             activeClass="active"
-    //             to="home"
-    //             spy={true}
-    //             smooth={true}
-    //             offset={-100}
-    //             duration={500}
-    //             className="header_link"
-    //             onClick={toggleMenu}
-    //           >
-    //             Home
-    //           </Link>
-
-    //           <Link
-    //             activeClass="active"
-    //             to="about"
-    //             spy={true}
-    //             smooth={true}
-    //             offset={-10}
-    //             duration={500}
-    //             className="header_link"
-    //             onClick={toggleMenu}
-    //           >
-    //             About
-    //           </Link>
-
-    //           <Link
-    //             activeClass="projects"
-    //             to="projects"
-    //             spy={true}
-    //             smooth={true}
-    //             offset={-5}
-    //             duration={500}
-    //             className="header_link"
-    //             onClick={toggleMenu}
-    //           >
-    //             Projects
-    //           </Link>
-
-    //           <Link
-    //             activeClass="contact"
-    //             to="contact"
-    //             spy={true}
-    //             smooth={true}
-    //             offset={-100}
-    //             duration={500}
-    //             className="header_link"
-    //             onClick={toggleMenu}
-    //           >
-    //             Contact
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </header>
-
     <nav className="navbar">
       <div className="navbar-container mr-16 ">
         <input
@@ -191,7 +45,7 @@ const Navbar = ({ toggleMenu, isOpen }) => {
           </li>
           <li>
             <Link
-              activeClass="projects"
+              activeClass="active"
               to="projects"
               spy={true}
               smooth={true}
@@ -204,11 +58,11 @@ const Navbar = ({ toggleMenu, isOpen }) => {
           </li>
           <li>
             <Link
-              activeClass="contact"
+              activeClass="active"
               to="contact"
               spy={true}
               smooth={true}
-              offset={-10}
+              offset={-5}
               duration={500}
               className="header_link"
             >
